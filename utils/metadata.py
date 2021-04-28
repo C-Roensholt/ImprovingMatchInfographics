e977794fbda6f84b4d7afa50acf08e8109f31ae6
Christian Rønsholt <ronsholt32@gmail.com>
2021-04-28 16:27:10 +0200

    created utils folder and metadata.py

--- utils/metadata.py
+++ utils/metadata.py
@@ -1,77 +1,86 @@
 # Images paths
 images = [
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Arsenal.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\AstonVilla.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Brighton.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Burnley.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Chelsea.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\CrystalPalace.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Everton.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Fulham.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Leeds.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Leicester.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Liverpool.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ManchesterCity.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ManchesterUnited.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Newcastle.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ShieffieldUnited.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Southampton.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Tottenham.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\WestBrom.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\WestHam.png",
-    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Wolves.png"
+    r"team_logos\Arsenal.png",
+    r"team_logos\AstonVilla.png",
+    r"team_logos\Brighton.png",
+    r"team_logos\Burnley.png",
+    r"team_logos\Chelsea.png",
+    r"team_logos\CrystalPalace.png",
+    r"team_logos\Everton.png",
+    r"team_logos\Fulham.png",
+    r"team_logos\Leeds.png",
+    r"team_logos\Leicester.png",
+    r"team_logos\Liverpool.png",
+    r"team_logos\ManchesterCity.png",
+    r"team_logos\ManchesterUnited.png",
+    r"team_logos\Newcastle.png",
+    r"team_logos\ShieffieldUnited.png",
+    r"team_logos\Southampton.png",
+    r"team_logos\Tottenham.png",
+    r"team_logos\WestBrom.png",
+    r"team_logos\WestHam.png",
+    r"team_logos\Wolves.png"
 ]
 
 
-# Home kit colors
-home_colors = {
-    'Arsenal': ,
-    'Aston Villa': ,
-    'Brighton': ,
-    'Burnley': ,
-    'Chelsea': ,
-    'Crystal Palace': ,
-    'Everton': ,
-    'Fulham': ,
-    'Leeds': ,
-    'Leicester': ,
-    'Liverpool': ,
-    'Manchester City': ,
-    'Manchester United': ,
-    'Newcastle': ,
-    'Sheffield United': ,
-    'Southampton': ,
-    'Tottenham': ,
-    'West Brom': ,
-    'West Ham': ,
-    'Wolves': 
-}
-
-# Away kit colors
-away_colors = {
-    'Arsenal': ,
-    'Aston Villa': ,
-    'Brighton': ,
-    'Burnley': ,
-    'Chelsea': ,
-    'Crystal Palace': ,
-    'Everton': ,
-    'Fulham': ,
-    'Leeds': ,
-    'Leicester': ,
-    'Liverpool': ,
-    'Manchester City': ,
-    'Manchester United': ,
-    'Newcastle': ,
-    'Sheffield United': ,
-    'Southampton': ,
-    'Tottenham': ,
-    'West Brom': ,
-    'West Ham': ,
-    'Wolves': 
-}
-
+# Team colors
+lightblue = '#1d91dc'
+red = '#e80942'
+yellow = '#f7a119'
+green = '#16C79A'
+turq = '#00e8dc'
+bordeaux = '#7f0000'
 
 # Stuff
 text_color = '#CECECD'
 facecolor = '#181818'
+
+# Home kit colors
+home_colors = {
+    'Arsenal': red,
+    'Aston Villa': bordeaux,
+    'Brighton': lightblue,
+    'Burnley': bordeaux,
+    'Chelsea': lightblue,
+    'Crystal Palace': red,
+    'Everton': lightblue,
+    'Fulham': red,
+    'Leeds': lightblue,
+    'Leicester': lightblue,
+    'Liverpool': red,
+    'Manchester City': lightblue,
+    'Manchester United': red,
+    'Newcastle': lightblue,
+    'Sheffield United': red,
+    'Southampton': red,
+    'Tottenham': lightblue,
+    'West Brom': lightblue,
+    'West Ham': bordeaux,
+    'Wolves': yellow
+}
+
+# Away kit colors
+away_colors = {
+    'Arsenal': lightblue,
+    'Aston Villa': lightblue,
+    'Brighton': yellow,
+    'Burnley': yellow,
+    'Chelsea': red,
+    'Crystal Palace': lightblue,
+    'Everton': yellow,
+    'Fulham': yellow,
+    'Leeds': yellow,
+    'Leicester': bordeaux,
+    'Liverpool': turq,
+    'Manchester City': green,
+    'Manchester United': yellow,
+    'Newcastle': yellow,
+    'Sheffield United': yellow,
+    'Southampton': yellow,
+    'Tottenham': yellow,
+    'West Brom': green,
+    'West Ham': lightblue,
+    'Wolves': bordeaux
+}
+
+
