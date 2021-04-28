78cde8f3e627557bbfd2c8c521495aed36947d32
Christian Rønsholt <ronsholt32@gmail.com>
2021-04-28 11:47:31 +0200

    added logos to plot

---
+++ utils/metadata.py
@@ -0,0 +1,77 @@
+# Images paths
+images = [
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Arsenal.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\AstonVilla.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Brighton.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Burnley.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Chelsea.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\CrystalPalace.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Everton.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Fulham.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Leeds.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Leicester.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Liverpool.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ManchesterCity.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ManchesterUnited.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Newcastle.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\ShieffieldUnited.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Southampton.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Tottenham.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\WestBrom.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\WestHam.png",
+    r"C:\Users\rcr1\Google Drive\football\pictures\premierLeague\Wolves.png"
+]
+
+
+# Home kit colors
+home_colors = {
+    'Arsenal': ,
+    'Aston Villa': ,
+    'Brighton': ,
+    'Burnley': ,
+    'Chelsea': ,
+    'Crystal Palace': ,
+    'Everton': ,
+    'Fulham': ,
+    'Leeds': ,
+    'Leicester': ,
+    'Liverpool': ,
+    'Manchester City': ,
+    'Manchester United': ,
+    'Newcastle': ,
+    'Sheffield United': ,
+    'Southampton': ,
+    'Tottenham': ,
+    'West Brom': ,
+    'West Ham': ,
+    'Wolves': 
+}
+
+# Away kit colors
+away_colors = {
+    'Arsenal': ,
+    'Aston Villa': ,
+    'Brighton': ,
+    'Burnley': ,
+    'Chelsea': ,
+    'Crystal Palace': ,
+    'Everton': ,
+    'Fulham': ,
+    'Leeds': ,
+    'Leicester': ,
+    'Liverpool': ,
+    'Manchester City': ,
+    'Manchester United': ,
+    'Newcastle': ,
+    'Sheffield United': ,
+    'Southampton': ,
+    'Tottenham': ,
+    'West Brom': ,
+    'West Ham': ,
+    'Wolves': 
+}
+
+
+# Stuff
+text_color = '#CECECD'
+facecolor = '#181818'
--- utils/probability_functions.py
+++ utils/probability_functions.py
@@ -6,13 +6,8 @@ import pandas as pd
 def poisson_distribution(k, lambd):
     return (lambd ** k * np.exp(-lambd)) / np.math.factorial(k)
 
-
-# Calculate probability for goals scored given expected goals
-#define maximum number of goals
-
 def score_probability(home_xG, away_xG, num_goals=10):
     
-    #loop through number of goals
     home_goals_dist = []
     away_goals_dist = []
     for goals_scored in range(num_goals):
@@ -23,23 +18,18 @@ def score_probability(home_xG, away_xG, num_goals=10):
         prob = poisson_distribution(k=goals_scored, lambd=away_xG)
         away_goals_dist.append([goals_scored, prob])
         
-    #concatenate probs
     x, y = zip(home_goals_dist[1], home_goals_dist[1])
     
-    # Calculate probability of scores
     score_probs = [[(x[0], y[0]), x[1]*y[1]] for x in home_goals_dist for y in away_goals_dist]
     score_probs.sort(key=lambda x: x[1], reverse=True)
     
     return home_goals_dist, away_goals_dist, score_probs
-
 
 # Calculate scoreline probabilities
 def win_loss_draw_probs(score_probs):
     draw_prob_list = []
     home_win_prob_list = []
     away_win_prob_list = []
-    #loop through scoreline and append home, away or draw scorelines
-    #i.e. (1-0, 1-1, 0-1)
     for score, prob in score_probs:
         if score[0] > score[1]:
             home_win_prob_list.append(prob)
@@ -51,8 +41,4 @@ def win_loss_draw_probs(score_probs):
     away_win_prob = sum(away_win_prob_list)
     draw_prob = sum(draw_prob_list)
     
-    return home_win_prob, away_win_prob, draw_prob
-
-#if __name__=="__main__":
-#    main()
-    
+    return home_win_prob, away_win_prob, draw_prob   
